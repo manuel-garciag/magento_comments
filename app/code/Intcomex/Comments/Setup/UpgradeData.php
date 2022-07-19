d41e2da7b449f6bcd191daadcbd10c78d6c91fbb
Manuel Garcia <manuel.garcia@intcomex.com>
2022-07-19 09:46:47 -0500

    0.1.2 - Creacion de data y ajuste en la tabla Se ajusta la tabla ya que no tenia autoincrementable y se agrega data a la tabla de estados de los comentarios

---
+++ app/code/Intcomex/Comments/Setup/UpgradeData.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Intcomex\Comments\Setup;
+
+use \Magento\Framework\Setup\UpgradeDataInterface;
+use \Magento\Framework\Setup\ModuleContextInterface;
+use \Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * Class UpgradeData
+ *
+ * @package Thecoachsmb\Mymodule\Setup
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+
+    /**
+     * Creates sample articles
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if ($context->getVersion()
+            && version_compare($context->getVersion(), '0.1.2') < 0
+        ) {
+            $tableName = $setup->getTable('comments_status');
+
+            $data = [
+                [
+                    'name' => 'Approved',
+                ],
+                [
+                    'name' => 'Pending',
+                ],
+                [
+                    'name' => 'Refused',
+                ],
+                [
+                    'name' => 'Delete',
+                ],
+            ];
+
+            $setup
+                ->getConnection()
+                ->insertMultiple($tableName, $data);
+        }
+
+        $setup->endSetup();
+    }
+}
